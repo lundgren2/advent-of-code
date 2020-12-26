c9acd1a2716350458eee392dd3b0d05e94ee129d
Tobias Lundgren <tobias@lundgren.design>
2020-12-26 01:35:24 +0100

    Add day 10 part 2

--- src/2020/day10/test.spec.ts
+++ src/2020/day10/test.spec.ts
@@ -1,43 +1,15 @@
 import {readInput} from '../../utils';
 
 import {part1} from './part1';
+import {part2} from './part2';
 
 describe('Advent of Code 2020 - Day 10', () => {
   let input: string;
-  const testInput = `28
-33
-18
-42
-31
-14
-46
-20
-48
-47
-24
-23
-49
-45
-19
-38
-39
-11
-1
-32
-25
-35
-8
-17
-7
-9
-4
-2
-34
-10
-3`;
+  let testInput: string;
 
   beforeAll(async () => {
     input = await readInput(__dirname + '/input');
+    testInput = await readInput(__dirname + '/test_input');
   });
 
   describe('part 1', () => {
@@ -61,6 +33,31 @@ describe('Advent of Code 2020 - Day 10', () => {
 
     it('should output 2112 from input', () => {
       expect(part1(input)).toBe(2112);
+    });
+  });
+
+  describe('part 2', () => {
+    it('should output 8', () => {
+      const input = `16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4`;
+      expect(part2(input)).toBe(8);
+    });
+
+    it('should output 19208 ', () => {
+      expect(part2(testInput)).toBe(19208);
+    });
+
+    it('should output 3022415986688 from input', () => {
+      expect(part2(input)).toBe(3022415986688);
     });
   });
 });
