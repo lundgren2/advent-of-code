bbb595672c30b8e622a8b92f5a1d380918074b05
Tobias Lundgren <tobias@lundgren.design>
2021-12-03 09:39:04 +0000

    Prepare for 2021

---
+++ src/2021/day00/test.spec.ts
@@ -0,0 +1,21 @@
+import {readInput} from '../../utils';
+
+import {part1} from './part1';
+
+describe('Advent of Code 2021 - Day x', () => {
+  let input: string;
+  beforeAll(async () => {
+    input = await readInput(__dirname + '/input');
+  });
+
+  describe('part 1', () => {
+    it('should output x ', () => {
+      const test_input = 'test';
+      expect(part1(test_input)).toBe('test');
+    });
+
+    it('should output correct from input', () => {
+      expect(part1(input)).toBe('test');
+    });
+  });
+});
